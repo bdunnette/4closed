33ddd81e9418c357bbf9c71d8e8f6d6b49da6426
Brian Dunnette <brian@dunnette.us>
2015-07-12 12:15:24 -0500

    switching to meteor-packaged version of cheerio

--- app/server/methods.js
+++ app/server/methods.js
@@ -10,7 +10,24 @@ Meteor.methods({
    */
    
    'getSheriffSales': function(){
-        var saleData = Scrape.website("http://www4.co.hennepin.mn.us/webforeclosure/resultbyname.asp");
-        console.log(saleData);
+     
+     var requestWithAgent = request.defaults({
+    headers: { "User-Agent": "Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0" }
+});
+        
+          try {
+    var res = requestWithAgent.postSync("http://www4.co.hennepin.mn.us/webforeclosure/resultbyname.asp");
+    if (res.response.statusCode == 200) {
+        console.log(res.body);
+        var $ = cheerio.load(res.body);
+        console.log($);
+        var table = $('td table');
+        console.log(table);
+        console.log(table['1']);
+    }
+} catch (error) {
+    // See below for info on errors
+    console.error(error);
+}
     }
 });
